4e629c8f4239d9a98e982b6e24ba8e129f6e84fb
rihanmohd <forlearningrihan@gmail.com>
2024-07-13 20:57:08 +0530

    MINI Calender
    Calender using js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-let inputs = document.getElementById("inp");
-let text = document.querySelector(".text");
- 
-function Add(){
-if(inputs.value == ""){
-    alert("Please Enter Task")
-}else {
-let newEle = document.createElement("ul");
-newEle.innerHTML=`${inputs.value} <i class="btnb">0</i>` ;
-text.appendChild(newEle);
-inputs.value = "";
-newEle.document.querySelector(".btnb").addEventListener("click", remove);
-function remove(){
-    newEle.remove()
-}
-}
-}
+const date = document.getElementById('date');
+const day = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+
+
+const today = new Date();
+
+
+const weekdays = ["sunday", "Monday", "Tuesday", "Wedensday", "Theresday", "Friday", "Saturday"];
+
+const allMonths = ["january", "February", "March", "April", "May", "June", "July", "Agust", "September", "Octuber", "November", "Desember"
+];
+
+date.innerHTML = (today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML = weekdays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
