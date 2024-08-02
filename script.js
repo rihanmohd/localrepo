7a39cea010e8cb6f26df1cabaefb3856868cabaa
rihanmohd <forlearningrihan@gmail.com>
2024-08-02 11:18:25 +0530

    Bakery web files
    files for bakery web

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
-// ============ SHOW MENU =================
+//  ================================= SHOW MENU ==================================//
 const navMenu = document.getElementById('nav-menu');
       navToggle = document.getElementById('nav-toggle');
-      navClose = document.getElementById('nav-close');
-    //    console.log(navClose)
-    //   Menu show
-    if(navToggle){
-        navToggle.addEventListener('click', ()=>{
-            console.log("hi")
-            navMenu.classList.add('show-menu')
+    navClose = document.getElementById('nav-close');
+
+
+    // Menu show//
+    if(navToggle) {
+        navToggle.addEventListener('click', () => {
+            console.log("h");
+            navMenu.classList.add('show--menu')
 
         })
+    }
 
-        }
-
-        // Menu hidden
-        if(navClose){
-            navClose.addEventListener('click', ()=>{
-                console.log("no")
-                navMenu.classList.remove('show-menu')
-            })
-        }
+    // Menu hidden//
+    if(navClose){
+        navClose.addEventListener('click', () =>{
+            console.log("j")
+            navMenu.classList.remove('show--menu')
+        })
+    }
 
 
-    //    ================= REMOVE MENU MOBILE =============
-    const navLink = document.querySelectorAll('.nav--link');
 
-    const linkAction = () => {
+    // ============================== REMOVE MENU MOBILE ==============================//
+    const navLink = document.querySelectorAll('.nav--link')
+
+    const Mine = () => {
         const navMenu = document.getElementById('nav-menu')
-        // when we click on each nav--link, we remove the show-menu class
         navMenu.classList.remove('show-menu')
+        console.log("log")
     }
-    navLink.forEach(n => n.addEventListener('click', linkAction))
+    navLink.forEach(n => n.addEventListener('click', Mine))
 
-
-
-    // =================== ADD SHADOW HEADER ===============
-    const shadowHeader = () =>{
+    // =================================== ADD BLUR HEADER ==============================//
+    const blurHeader = () => {
         const header = document.getElementById('header')
-        // Add a class if the bottom is > 50 of vh
-        this.scrollY >= 50 ? header.classList.add('shadow-header')
-           : header.classList.remove('shadow-header')
+        // when the scroll is > 50 vh add the blur-header class to the header
+          this.scrollY >= 50 ? header.classList.add('blur-header')
+                               : header.classList.remove('blur-header')
+
+
     }
 
-    window.addEventListener('scroll' , shadowHeader)
+    window.addEventListener('scroll', blurHeader)
 
 
-    // ============================ SHOW SCROLL UP =============================
+
+    // ========================== SHOW SCROLL UP ====================//
     const scrollUp = () => {
         const scrollUp = document.getElementById('scroll-up')
-        // When the scroll is higher than 350 vh, add the 
-        this.scrollY >= 350 ? scrollUp.classList.add(show-scroll)
-                            :scrollUp.classList.remove('show-scroll')
-                            
+        // when the scroll is > than 350 vh add the
+        this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                               : scrollUp.classList.remove('show-scroll')
     }
+
     window.addEventListener('scroll', scrollUp)
 
-
-
-    // ==================== SCROLL SECTION ACTIVE LINK ======================
+    // ======================= SCROLL SECTIONS ACTIVE LINK ====================//
     const sections = document.querySelectorAll('section[id]')
 
     const scrollActive = () => {
@@ -65,37 +65,46 @@ const navMenu = document.getElementById('nav-menu');
 
         sections.forEach(current => {
             const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop -58,
-            sectionId = current.getAttribute('id'),
-            sectionsClass = document.querySelectorAll('.nav--menu a[href*=' + sectionId + ']')
+                   sectionTop = current.offsetTop - 58
+                   sectionId = current.getAttribute('id'),
+                   sectionsClass = document.querySelector('.nav--menu a [href*=' + sectionId + ']')
+              
 
-            if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-                sectionsClass.classList.add('active-link')
-            }else{
-                sectionsClass.classList.remove('active-link')
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
 
-            }
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
         })
     }
-    window.addEventListener('scroll',  scrollActive)
 
-    // ============== SCROLL  REVEAL ANIMATION ================>
-        const sr = ScrollReveal({
-            origin: 'top',
-            distance: '60px',
-            duration: 2500,
-            delay: 300,
-            // reset: true, // Animations repeat
+    window.addEventListener('scroll', scrollActive)
 
-        })
+    // ====================== SCROLL REVEAL ANIMATION ===========//
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '40px',
+        opacity: 1,
+        scale: 1.1,
+        duration: 2500,
+        delay: 300,
+      //  reset: true,  //Animation repeat
 
-        sr.reveal(`.home--data, .footer`)
-        sr.reveal(`.home--dish`, {delay: 500, distance: '100px', origin: 'bottom'})
-        sr.reveal(`.home--burger`, {delay: 1200, distance: '100px', duration: 1500})
-        sr.reveal(`.home--ingrident`, {delay: 1600, interval: 100})
-        sr.reveal(`.recipe--img, .delivery--img, .contact--image`, {origin: 'left'})
-        sr.reveal(`.recipe--data, .delivery--data, .contact--data`, {origin: 'right'})
-        sr.reveal(`.populer--card`, {interval: 100})
-    
-    
-    
+    })
+
+    sr.reveal(`.home--data, .about--img, .about--data, .visit--data`)
+
+    sr.reveal(`.home--image, .footer--img-1, .footer--img-2`, { rotate: {z: -15} })
+    sr.removel(`.home--bread, .about--bread`, { rotate: {z: 15}})
+    sr.removel(`.home--footer`, { scale: 1, origin: 'bottom' })
+
+    sr.reveal(`.new--card:nth-child(1) img`, {rotate: {z: -30}, distance: 0})
+    sr.reveal(`.new--card:nth-child(2) img`, {rotate: {z: 15}, distance: 0, delay: 600 })
+    sr.reveal(`.new--card:nth-child(3) img`, {rotate: {z: -30}, distance: 0, delay: 900 })
+
+
+    sr.reveal(`.favorite--card img`, { interval: 100,rotate: {z: 15}, distance: 0})
+
+    sr.reveal(`.footer--container`, { scale: 1})
+
